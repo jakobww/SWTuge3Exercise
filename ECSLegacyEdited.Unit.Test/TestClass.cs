ea4929dc145eb16b5b728b663a1da2412d8a654d
jakobww <jakobwilbrandt@gmail.com>
2018-09-17 13:54:53 +0200

    changing test

--- ECSLegacyEdited.Unit.Test/TestClass.cs
+++ ECSLegacyEdited.Unit.Test/TestClass.cs
@@ -11,25 +11,21 @@ namespace ECSLegacyEdited.Unit.Test
     class TestClass
     {
 
-        /*[Test]
+        [Test]
         TurnOnHeater_TurnOnWhen25Degrees_WriteHeaterON()
         {
 
-            FakeHeater myHeater;
-            FakeTempSensor mySensor;
-            // Create a mock of your interface and make the methods verifiable.
-            var mock = new ECS(23,myHeater,mySensor);
 
-            mock.Setup(m => m.TurnOn())
-                .Verifiable();
+            
+            IHeater myHeater = new FakeHeater();
+            ITempSensor myTempSensor = new FakeTempSensor();
 
-            // Setup your class which you expect to be calling the verifiable method
-            var classToTest = new ECS();
-            classToTest.DoWork();
 
-            // Verify the method is called
-            mock.Verify(m => m.DoSomething());
-        }*/
+            
+            
+
+
+        }
 
     }
 }
